1e450d01a3be86f30ae31c8ceadd0068eabd086c
alex.fanning <alexfanning14@gmail.com>
2017-08-04 16:00:43 +0100

    Widget Displaying - Intent not working

--- app/src/main/java/com/udacity/stockhawk/StockWidgetProvider.java
+++ app/src/main/java/com/udacity/stockhawk/StockWidgetProvider.java
@@ -5,22 +5,87 @@ import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.widget.RemoteViews;
 
 import com.udacity.stockhawk.ui.MainActivity;
+import com.udacity.stockhawk.ui.StockDetailsActivity;
 
 /**
  * Created by alex.fanning on 03/08/2017.
  */
 
 public class StockWidgetProvider extends AppWidgetProvider {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        int appWidgetId = intent.getIntExtra(AppWidgetManager.EXTRA_APPWIDGET_ID,AppWidgetManager.INVALID_APPWIDGET_ID);
+        Intent i = new Intent(context,StockDetailsActivity.class);
+        context.startActivity(i);
+        super.onReceive(context, intent);
+    }
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        for (int appWidgetID : appWidgetIds){
+            updateAppWidget(context,appWidgetManager,appWidgetID);
+        }
+
+
+
+    }
+
+
+
+
+    @Override
+    public void onDeleted(Context context, int[] appWidgetIds) {
+        super.onDeleted(context, appWidgetIds);
+    }
+
+    @Override
+    public void onEnabled(Context context) {
+        super.onEnabled(context);
+    }
+
+    @Override
+    public void onDisabled(Context context) {
+        super.onDisabled(context);
+    }
 
     static void updateAppWidget(Context c, AppWidgetManager awm, int appWidgetId){
-        Intent i = new Intent(c, MainActivity.class);
-        PendingIntent pendingIntent = PendingIntent.getActivity(c,0,i,0);
+
         RemoteViews views = new RemoteViews(c.getPackageName(),R.layout.widget_list_view);
-        
+        Intent i = new Intent(c,StockWidgetService.class);
+        views.setRemoteAdapter(R.id.widget_list_view,i);
+
+        Intent appIntent = new Intent(c, StockDetailsActivity.class);
+        appIntent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID,appWidgetId);
+        appIntent.setData(Uri.parse(appIntent.toUri(Intent.URI_INTENT_SCHEME)));
+        PendingIntent pendingIntent = PendingIntent.getActivity(c,appWidgetId,appIntent,PendingIntent.FLAG_UPDATE_CURRENT);
+        views.setPendingIntentTemplate(R.id.symbol_widget,pendingIntent);
+        awm.updateAppWidget(appWidgetId,views);
     }
 
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
